5586bcf31a22c8f62ad21ade694f1a4df9a9c7e8
1krishna <krishrockstar1@gmail.com>
2020-08-02 13:01:46 +0530

    Update header.php

--- farmer/includes/header.php
+++ farmer/includes/header.php
@@ -1,46 +1,54 @@
 	<script src="js/custom.js"></script>
+	<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
+	<script type="text/javascript">
+		function googleTranslateElementInit() {
+			new google.translate.TranslateElement({
+				pageLanguage: 'en'
+			}, 'google_translate_element');
+		}
+	</script>
 	<header class="header">
-	    <div class="logo-container">
-	        <a href="dashboard.php" class="logo">
-	            <img src="img/logo.png" width="130" height="40" alt="Porto Admin" />
-	        </a>
-	        <div class="d-md-none toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened">
-	            <i class="fas fa-bars" aria-label="Toggle sidebar"></i>
-	        </div>
-	    </div>
-	    <?php
-        if (!isset($_SESSION['far_id']) || isset($_GET['lo'])) {
-            session_destroy();
-            header('Location: ../falogin.php');
-        }
-        $far = "SELECT * FROM `farmers` WHERE far_id=" . $_SESSION['far_id'];
-        $far = mysqli_query($conn, $far);
-        $far = mysqli_fetch_assoc($far);
-        ?>
-	    <!-- start: search & user box -->
-	    <div class="header-right">
-	        <span class="separator"></span>
-	        <div id="userbox" class="userbox">
-	            <a href="#" data-toggle="dropdown">
-	                <div class="profile-info" data-lock-name="John Doe" data-lock-email="johndoe@okler.com">
-	                    <span class="name">
-						<?php
-						echo $far['far_name']; ?></span>
-	                </div>
+		<div class="logo-container">
+				<div id="google_translate_element"></div>
+			<div class="d-md-none toggle-sidebar-left" data-toggle-class="sidebar-left-opened" data-target="html" data-fire-event="sidebar-left-opened">
+				<i class="fas fa-bars" aria-label="Toggle sidebar"></i>
+			</div>
 
-	                <i class="fa custom-caret"></i>
-	            </a>
+		</div>
+		<?php
+		if (!isset($_SESSION['far_id']) || isset($_GET['lo'])) {
+			session_destroy();
+			header('Location: ../falogin.php');
+		}
+		$far = "SELECT * FROM `farmers` WHERE far_id=" . $_SESSION['far_id'];
+		$far = mysqli_query($conn, $far);
+		$far = mysqli_fetch_assoc($far);
+		?>
+		<!-- start: search & user box -->
+		<div class="header-right">
 
-	            <div class="dropdown-menu">
-	                <ul class="list-unstyled mb-2">
-	                    <li class="divider"></li>
-	                    
-	                    <li>
+			<span class="separator"></span>
+			<div id="userbox" class="userbox">
+				<a href="#" data-toggle="dropdown">
+					<div class="profile-info" data-lock-name="John Doe" data-lock-email="johndoe@okler.com">
+						<span class="name">
+							<?php
+							echo $far['far_name']; ?></span>
+					</div>
+
+					<i class="fa custom-caret"></i>
+				</a>
+
+				<div class="dropdown-menu">
+					<ul class="list-unstyled mb-2">
+						<li class="divider"></li>
+
+						<li>
 							<a role="menuitem" tabindex="-1" href="<?php echo $_SERVER['PHP_SELF'] . "?lo"; ?>"><i class="fas fa-power-off"></i> Logout</a>
-	                    </li>
-	                </ul>
-	            </div>
-	        </div>
-	    </div>
-	  </header>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
